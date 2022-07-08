dbb221b07f548a882fef012641e07d80e0de831c
fentosv <victortic0910@gmail.com>
2022-07-08 12:16:16 +0200

    Fixes

--- index.js
+++ index.js
@@ -4,74 +4,100 @@ const figlet = require('figlet');
 const chalk = require('chalk');
 const { clear } = require('console');
 
-clear();
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
 
-console.log(
-    chalk.red(
-        figlet.textSync('Header', {
-            horizontalLayout: 'default',
-            font: "ogre",
-            whitespaceBreak: true
-        }))
-);
+const CliFunction = async () => {
 
-console.log(
-    chalk.red(
-        figlet.textSync('               converter', {
-            horizontalLayout: 'default',
-            font: "ogre",
-            whitespaceBreak: true
-        }))
-);
+    clear();
 
+    console.log(
+        chalk.red(
+            figlet.textSync('Header', {
+                horizontalLayout: 'default',
+                font: "ogre",
+                whitespaceBreak: true
+            }))
+    );
 
-console.log(chalk.red("Desarrollado por Unworking.\n\n"));
+    console.log(
+        chalk.red(
+            figlet.textSync('               converter', {
+                horizontalLayout: 'default',
+                font: "ogre",
+                whitespaceBreak: true
+            }))
+    );
 
-console.log(chalk.green('[INSTRUCCIONES]\n'));
-console.log(chalk.green('Introduce los headers en la misma carpeta que este script, con el nombre "headers.txt".\n\n\n'));
+    console.log(chalk.red("Desarrollado por Fentosv.\n\n"));
 
+    console.log(chalk.green('[INSTRUCCIONES]\n'));
+    console.log(chalk.green('Introduce los headers en la misma carpeta que este ejecutable, con el nombre "headers.txt".\n\n\n'));
 
-inquirer.prompt([
-    {
-        name: 'outputType',
-        type: 'list',
-        message: 'Elige una de las formas de visualización de los headers:\n\n',
-        choices: ["'Encabezado' : 'Valor'", "Encabezado : 'Valor'"],
-        default: 0,
-        prefix: chalk.red(">>>"),
-    }]).then((answers) => {
+    inquirer.prompt([
+        {
+            name: 'outputType',
+            type: 'list',
+            message: 'Elige una de las formas de visualización de los headers:\n\n',
+            choices: ["'Encabezado' : 'Valor'", "Encabezado : 'Valor'"],
+            default: 0,
+            prefix: chalk.red(">>>"),
+        }]).then((answers) => {
 
-        const url = "./headers.txt"
-        const data = fs.readFileSync(url, 'utf-8').trim().split("\n");
+            const url = "./headers.txt"
+            const data = fs.readFileSync(url, 'utf-8').trim().split("\n");
+            let output = ''
 
-        if (answers.outputType === "'Encabezado' : 'Valor'") {
-            //Leemos txt, quitamos espacios en blanco y hacemos array (cada línea es un elemento)
-            console.log("\n");
+            if (answers.outputType === "'Encabezado' : 'Valor'") {
+                //Leemos txt, quitamos espacios en blanco y hacemos array (cada línea es un elemento)
+                console.log("\n");
+                console.log("HEADERS CON FORMATO:");
+                console.log("\n");
 
-            for (const line of data) {
+                for (const line of data) {
 
-                if (data.indexOf(line) < data.length - 1) {
-                    console.log(`'${line.split(":")[0]}' : '${line.split(":")[1].trim()}',`);
+                    if (data.indexOf(line) < data.length - 1) {
+                        // console.log(`'${line.split(":")[0]}' : '${line.split(":")[1].trim()}',`);
+                        output += `'${line.split(":")[0]}' : '${line.split(":")[1].trim()}',\n`;
 
-                } else {
-                    console.log(`'${line.split(":")[0]}' : '${line.split(":")[1].trim()}'`)
+                    } else {
+                        // console.log(`'${line.split(":")[0]}' : '${line.split(":")[1].trim()}'`)
+                        output += `'${line.split(":")[0]}' : '${line.split(":")[1].trim()}'\n`
+                    }
+                }
+
+            } else {
+                for (const line of data) {
+
+                    if (data.indexOf(line) < data.length - 1) {
+                        // console.log(`${line.split(":")[0]} : '${line.split(":")[1].trim()}',`);
+                        output += `${line.split(":")[0]} : '${line.split(":")[1].trim()}',\n`;
+
+                    } else {
+                        // console.log(`${line.split(":")[0]} : '${line.split(":")[1].trim()}'`)
+                        output += `${line.split(":")[0]} : '${line.split(":")[1].trim()}'\n`
+                    }
                 }
             }
+            console.log(output);
+            fs.writeFileSync('result.txt', output)
+        });
 
-        } else {
-            for (const line of data) {
+    await sleep(4000)
 
-                if (data.indexOf(line) < data.length - 1) {
-                    console.log(`${line.split(":")[0]} : '${line.split(":")[1].trim()}',`);
+}
 
-                } else {
-                    console.log(`${line.split(":")[0]} : '${line.split(":")[1].trim()}'`)
-                }
+const resolve = async () => {
 
-            }
-        }
+    await CliFunction()
 
-    });
+
+}
+
+resolve()
+
+
 
 
 
